092db8df735c0ed8e67add8cd743f47a7ee71795
Jelle <Jelle@Jelle-PC.home>
2017-06-01 00:09:44 +0200

    practicum 8 --> alle landen kunnen worden opgehaald en de wijzigingspagina voor de landen zijn gemaakt. nu nog data erinstoppen

--- firstapp/src/main/webapp/script/8_1.js
+++ firstapp/src/main/webapp/script/8_1.js
@@ -1,37 +1,196 @@
 $(document).ready(function(){
 	
-	
+	//verbergen van elementen
 	$('#toevoegen').hide();
 	$('#search').hide();
-
-	//home-knop
-	$("#menu-home").click(function(){
-		$('#home').hide();
-		$('#toevoegen').hide();
-		$('#search').hide();
-		$('#home').fadeIn(1000);
-	});
-
-
-	//add-knop
-	$('#menu-item1').click(function(){
-		$('#home').hide();
-		$('#toevoegen').hide();
-		$('#search').hide();
-		$('#toevoegen').fadeIn(1000);
-		
-	});
-
-	//search-knop
-	$('#menu-item2').click(function(){
-		$('#home').hide();
-		$('#toevoegen').hide();
-		$('#search').hide();	
-		$('#search').fadeIn(1000);
-		
-	});
+	$('#home').hide();  							/*tijdelijk  om  de inhoud te bepalen*/	
 	
 	
+
+	
+//einde startup	
 });
+	
+	
+
+
+//home-knop
+$("#menu-home").click(function(){
+	$('#home').hide();
+	$('#toevoegen').hide();
+	$('#search').hide();
+	$('#home').fadeIn(1000);
+});
+
+
+//add-knop
+$('#menu-item1').click(function(){
+	$('#home').hide();
+	$('#toevoegen').hide();
+	$('#search').hide();
+	$('#toevoegen').fadeIn(1000);
+});
+
+//search-knop
+$('#menu-item2').click(function(){
+	$('#home').hide();
+	$('#toevoegen').hide();
+	$('#search').hide();	
+	$('#search').fadeIn(1000);
+});
+	
+
+	
+	
+	
+	
+/*
+	=================================================
+		-1-			Functie: 	1  Land ophalen
+	=================================================
+	
+*/
+	
+function landOphalen(code){
+		
+		alert('dit werkt!: ' + code);
+		
+		$.getJSON("http://localhost:8070/firstapp/restservices/countries/"+code,+"jsonp" ,function(data){			
+
+			
+		// vullen van de section met id="land-pagina"
+			
+			
+			
+			
+			
+			
+	}),"json"}
+		
+	
+	
+	
+/*
+	=================================================
+		-2-			Functie: 	Alle landen ophalen
+	=================================================
+	
+*/	
+	
+function alleLanden(){
+		
+
+	$.getJSON("http://localhost:8070/firstapp/restservices/countries","jsonp" ,function(data){			
+		$.each(data, function(k, v){
+			$('tbody').append("<tr onclick= 'landOphalen(\"" + v.code + "\");'><td>"+v.Land+"</td><td>"+v.code+"</td><td>"+v.capital+"</td><td>"+v.continent+"</td><td>"+v.region+"</td><td>"+v.surface+"</td><td>"+v.population+"</td><td>"+v.government+"</td><td>"+v.latitude+"</td><td>"+v.longitude+"</td></tr>");
+		});			
+}),"json"}
+	
+
+
+
+
+	
+	
+	
+/*
+	=================================================
+		-3-			Functie: 	Oppervlakte ophalen
+	=================================================
+	
+*/	
+		
+function grootsteOppervlaktes(){
+		
+	alert('oppervlaktes');
+}
+	
+	
+	
+	
+/*
+	=================================================
+		-4-			Functie: 	Populatie ophalen
+	=================================================
+	
+*/	
+		
+	
+function grootstePopulaties(){
+		
+		
+	alert('populaties');
+}
+	
+	
+	
+/*
+	=================================================
+		-5-			Functie: 	RandomSearch
+	=================================================
+	
+*/
+	
+	
+	
+	
+	
+	
+	
+/*
+	=================================================
+		-6-			Functie: 	Land aanmaken		==> POST REQUEST
+	=================================================
+	
+*/	
+	
+	
+	
+	
+	
+	/*
+	=================================================
+		-7-			Functie: 	Land Verwijderen   ==>	DELETE REQUEST
+	=================================================
+	
+*/	
+	
+		
+	
+
+	
+	
+	
+	/*
+	=================================================
+		-8-			Functie: 	Land wijzigen 		==>  PUT REQUEST
+	=================================================
+	
+*/	
+	
+		
+
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
 
 
