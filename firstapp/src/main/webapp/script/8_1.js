d8ae54a56081da020245c1584327c40bf181edaf
Jelle <Jelle@Jelle-PC.home>
2017-05-31 17:45:56 +0200

    Practicum 8 --> alle html/css is klaar. nu alleen nog javascript om alle data op te halen om er een echt werkend iets van te maken

--- firstapp/src/main/webapp/script/8_1.js
+++ firstapp/src/main/webapp/script/8_1.js
@@ -1,43 +1,37 @@
 $(document).ready(function(){
 	
-	alert("ready?");
 	
 	$('#toevoegen').hide();
 	$('#search').hide();
-	
-	var left  = "<<<";
-	var right = ">>>";
-	document.getElementById("label-left").innerHTML = left;
-	document.getElementById("label-right").innerHTML = right;
-	
+
+	//home-knop
+	$("#menu-home").click(function(){
+		$('#home').hide();
+		$('#toevoegen').hide();
+		$('#search').hide();
+		$('#home').fadeIn(1000);
+	});
+
+
+	//add-knop
+	$('#menu-item1').click(function(){
+		$('#home').hide();
+		$('#toevoegen').hide();
+		$('#search').hide();
+		$('#toevoegen').fadeIn(1000);
+		
+	});
+
+	//search-knop
+	$('#menu-item2').click(function(){
+		$('#home').hide();
+		$('#toevoegen').hide();
+		$('#search').hide();	
+		$('#search').fadeIn(1000);
+		
+	});
 	
 	
 });
 
-//home-knop
-$('#menu-home').click(function(){
-	$('#home').hide();
-	$('#toevoegen').hide();
-	$('#search').hide();
-	$('#home').fadeIn(1000);
-})
 
-
-//add-knop
-$('#menu-item1').click(function(){
-	$('#home').hide();
-	$('#toevoegen').hide();
-	$('#search').hide();
-	$('#toevoegen').fadeIn(1000);
-	
-})
-
-//search-knop
-$('#menu-item2').click(function(){
-	$('#home').hide();
-	$('#toevoegen').hide();
-	$('#search').hide();
-	
-	$('#search').fadeIn(1000);
-	
-})
