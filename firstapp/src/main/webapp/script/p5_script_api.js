02c6f616946fd2fd3ec9599508dc192916457d1d
Jelle <Jelle@Jelle-PC.home>
2017-05-31 04:29:01 +0200

    design is nu responsive --> practicum 6 afgerond

--- firstapp/src/main/webapp/script/p5_script_api.js
+++ firstapp/src/main/webapp/script/p5_script_api.js
@@ -26,7 +26,7 @@ function initPage(){
 		 document.getElementById("land").innerHTML = data.country;
 		 document.getElementById("regio").innerHTML = data.region;
 		 document.getElementById("stad").innerHTML =  "<div class=\"stad\" onclick= 'weather(\"" + data.lat + "\", \"" + data.lon + "\", \""  + data.city + "\");'>" + data.city + "</div>";
-		 document.getElementById("postcode").innerHTML = data.zip;
+		 document.getElementById("postcode").innerHTML = "code: " + data.zip;
 		 document.getElementById("lat").innerHTML = data.lat;
 		 document.getElementById("lon").innerHTML = data.lon;
 		 document.getElementById("ip").innerHTML = data.query;
@@ -96,7 +96,7 @@ function weather(latitude, longtitude,city){
 				document.getElementById("temperatuur").innerHTML = data.main.temp + " \u00b0C";
 				document.getElementById("luchvochtigheid").innerHTML = data.main.humidity + "%";
 				document.getElementById("windsnelheid").innerHTML = (data.wind.speed /3.6) + "m/s";
-				document.getElementById("windrichting").innerHTML = data.wind.deg;
+				document.getElementById("windrichting").innerHTML = degToCard(data.wind.deg);
 				document.getElementById("zonsopgang").innerHTML = stringToTime(data.sys.sunrise);
 				document.getElementById("zonsondergang").innerHTML = stringToTime(data.sys.sunset);
 				document.getElementById("location").innerHTML = (data.name);
@@ -169,7 +169,7 @@ function weather(latitude, longtitude,city){
 			document.getElementById("temperatuur").innerHTML = data.main.temp + " \u00b0C";
 			document.getElementById("luchvochtigheid").innerHTML = data.main.humidity + "%";
 			document.getElementById("windsnelheid").innerHTML = (data.wind.speed /3.6) + "m/s";
-			document.getElementById("windrichting").innerHTML = data.wind.deg;
+			document.getElementById("windrichting").innerHTML = degToCard(data.wind.deg);
 			document.getElementById("zonsopgang").innerHTML = stringToTime(data.sys.sunrise);
 			document.getElementById("zonsondergang").innerHTML = stringToTime(data.sys.sunset);
 			document.getElementById("location").innerHTML = (data.name);
@@ -208,7 +208,7 @@ function weather(latitude, longtitude,city){
 		document.getElementById("temperatuur").innerHTML = data.main.temp + " \u00b0C";
 		document.getElementById("luchvochtigheid").innerHTML = data.main.humidity + "%";
 		document.getElementById("windsnelheid").innerHTML = (data.wind.speed /3.6) + "m/s";
-		document.getElementById("windrichting").innerHTML = data.wind.deg;
+		document.getElementById("windrichting").innerHTML = degToCard(data.wind.deg);
 		document.getElementById("zonsopgang").innerHTML = stringToTime(data.sys.sunrise);
 		document.getElementById("zonsondergang").innerHTML = stringToTime(data.sys.sunset);
 		document.getElementById("location").innerHTML = (data.name);
@@ -373,4 +373,43 @@ function parseDate(input) {
   // new Date(year, month [, date [, hours[, minutes[, seconds[, ms]]]]])
   return new Date(parts[0], parts[1]-1, parts[2], parts[3], parts[4], parts[5], parts[6]); // months are 0-based
 }
+
+
+
+
+function degToCard(deg){
+	  if (deg>11.25 && deg<33.75){
+	    return "Noord / Noord-Oost";
+	  }else if (deg>33.75 && deg<56.25){
+	    return "Noord-Oost";
+	  }else if (deg>56.25 && deg<78.75){
+	    return "Oost / Noord-Oost";
+	  }else if (deg>78.75 && deg<101.25){
+	    return "Oost";
+	  }else if (deg>101.25 && deg<123.75){
+	    return "Oost Zuid-Oost";
+	  }else if (deg>123.75 && deg<146.25){
+	    return "Zuid-Oost";
+	  }else if (deg>146.25 && deg<168.75){
+	    return "Zuid Zuid-Oost";
+	  }else if (deg>168.75 && deg<191.25){
+	    return "Zuid";
+	  }else if (deg>191.25 && deg<213.75){
+	    return "Zuid Zuid-West";
+	  }else if (deg>213.75 && deg<236.25){
+	    return "Zuid-West";
+	  }else if (deg>236.25 && deg<258.75){
+	    return "West Zuid-West";
+	  }else if (deg>258.75 && deg<281.25){
+	    return "West";
+	  }else if (deg>281.25 && deg<303.75){
+	    return "West Noord-West";
+	  }else if (deg>303.75 && deg<326.25){
+	    return "Noord-West";
+	  }else if (deg>326.25 && deg<348.75){
+	    return "Nootd Noord-West";
+	  }else{
+	    return "Noord"; 
+	  }
+	}
 
